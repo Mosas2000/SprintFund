7dce76cc393afae2f15735dc8fc9a2570656bec7
0xMosas <tosinolosasa@gmail.com>
2026-03-21 14:47:12 +0100

    Add charts components index and barrel exports

---
+++ frontend/components/charts/index.ts
@@ -0,0 +1,10 @@
+// Chart and data visualization components
+export { default as CategoryChart } from './CategoryChart';
+export { default as VotingTrendsChart } from './VotingTrendsChart';
+export { default as TreasuryBalanceChart } from './TreasuryBalanceChart';
+export { default as ProposerActivityChart } from './ProposerActivityChart';
+export { default as SuccessRateChart } from './SuccessRateChart';
+export { default as SocialGraph } from './SocialGraph';
+export { default as EcosystemBenchmarks } from './EcosystemBenchmarks';
+export { default as BudgetAllocator } from './BudgetAllocator';
+export { default as ExpenseTracker } from './ExpenseTracker';
