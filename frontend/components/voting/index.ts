7898d4a36b4d1f956663147157f6e6152d61da99
0xMosas <tosinolosasa@gmail.com>
2026-03-21 14:46:24 +0100

    Add voting components index and barrel exports

---
+++ frontend/components/voting/index.ts
@@ -0,0 +1,11 @@
+// Voting-related components
+export { default as VoteDelegation } from './VoteDelegation';
+export { default as DelegatorCard } from './DelegatorCard';
+export { default as DelegatorMarketplace } from './DelegatorMarketplace';
+export { default as BulkVotingQueue } from './BulkVotingQueue';
+export { default as VoterInfluence } from './VoterInfluence';
+export { default as DelegationStats } from './DelegationStats';
+export { default as VotingAnalyticsDashboard } from './VotingAnalyticsDashboard';
+export { default as VotingTrendsChart } from './VotingTrendsChart';
+export { default as VotingProgressBar } from './VotingProgressBar';
+export { default as QuorumMonitor } from './QuorumMonitor';
