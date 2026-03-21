c671a22e38c2850a6e001d4343c524acf76019fc
0xMosas <tosinolosasa@gmail.com>
2026-03-21 14:46:38 +0100

    Add dashboard components index and barrel exports

---
+++ frontend/components/dashboard/index.ts
@@ -0,0 +1,11 @@
+// Dashboard-related components
+export { default as UserDashboard } from './UserDashboard';
+export { default as DashboardCustomizer } from './DashboardCustomizer';
+export { default as UserProfile } from './UserProfile';
+export { default as UserInsights } from './UserInsights';
+export { default as UserNetwork } from './UserNetwork';
+export { default as ActivityFeed } from './ActivityFeed';
+export { default as AuditTrail } from './AuditTrail';
+export { default as PerformanceMetricsPanel } from './PerformanceMetricsPanel';
+export { default as AnalyticsKPIPanel } from './AnalyticsKPIPanel';
+export { default as ImpactAssessment } from './ImpactAssessment';
