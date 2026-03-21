3cb301fd12409d98d32124a9a4d53d661e2d37c3
0xMosas <tosinolosasa@gmail.com>
2026-03-21 13:34:18 +0100

    Add error boundaries barrel export file

---
+++ frontend/src/components/error-boundaries/index.ts
@@ -0,0 +1,16 @@
+export { SectionErrorBoundary } from '../SectionErrorBoundary';
+export { ProposalListErrorBoundary } from '../ProposalListErrorBoundary';
+export { AnalyticsDashboardErrorBoundary } from '../AnalyticsDashboardErrorBoundary';
+export { DashboardStatsErrorBoundary } from '../DashboardStatsErrorBoundary';
+export { VotingInterfaceErrorBoundary } from '../VotingInterfaceErrorBoundary';
+export { TransactionHistoryErrorBoundary } from '../TransactionHistoryErrorBoundary';
+export { FormComponentErrorBoundary } from '../FormComponentErrorBoundary';
+export { PaginationErrorBoundary } from '../PaginationErrorBoundary';
+
+export { ProposalListErrorFallback } from '../ProposalListErrorFallback';
+export { AnalyticsErrorFallback } from '../AnalyticsErrorFallback';
+export { DashboardStatsErrorFallback } from '../DashboardStatsErrorFallback';
+export { VotingInterfaceErrorFallback } from '../VotingInterfaceErrorFallback';
+export { TransactionHistoryErrorFallback } from '../TransactionHistoryErrorFallback';
+export { FormComponentErrorFallback } from '../FormComponentErrorFallback';
+export { PaginationErrorFallback } from '../PaginationErrorFallback';
