72380a0550055efbe7a79de73d03dd12a0755719
0xMosas <tosinolosasa@gmail.com>
2026-03-21 14:46:07 +0100

    Add proposals components index and barrel exports

---
+++ frontend/components/proposals/index.ts
@@ -0,0 +1,11 @@
+// Proposal-related components
+export { default as ProposalList } from './ProposalList';
+export { default as ProposalDetailError } from './ProposalDetailError';
+export { default as ProposalSortbar } from './ProposalSortbar';
+export { default as ProposalLink } from './ProposalLink';
+export { default as PaginatedProposalList } from './PaginatedProposalList';
+export { default as RelatedProposals } from './RelatedProposals';
+export { default as ProposalArchive } from './ProposalArchive';
+export { default as ProposalCollaboration } from './ProposalCollaboration';
+export { default as ProposalRevocation } from './ProposalRevocation';
+export { default as ProposalAnalytics } from './ProposalAnalytics';
