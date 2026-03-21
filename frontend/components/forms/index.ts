4f83cdd646939a5071be0d7452aaf5c47af62b74
0xMosas <tosinolosasa@gmail.com>
2026-03-21 14:47:30 +0100

    Add forms components index and barrel exports

---
+++ frontend/components/forms/index.ts
@@ -0,0 +1,8 @@
+// Form components
+export { default as CreateProposalForm } from './CreateProposalForm';
+export { default as MarkdownEditor } from './MarkdownEditor';
+export { default as CommentInput } from './CommentInput';
+export { default as DiscussionComment } from './DiscussionComment';
+export { default as DiscussionSearch } from './DiscussionSearch';
+export { default as CategoryManager } from './CategoryManager';
+export { default as DateRangeFilter } from './DateRangeFilter';
