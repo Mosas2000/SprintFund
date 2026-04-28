350fbd2137e4bca7f1dcaafe992e2d1ebf2b415e
0xMosas <tosinolosasa@gmail.com>
2026-04-28 18:41:46 +0100

    update exports to include new helper functions

--- scripts/lib/index.js
+++ scripts/lib/index.js
@@ -1,1 +1,1 @@
-export { loadContractConfig, getContractAddress, getContractName, getContractPrincipal, getNetworkConfig, getContractVersion } from './contract-config.js';
+export { loadContractConfig, getContractAddress, getContractName, getContractPrincipal, getNetworkConfig, getContractVersion, getLegacyContractName, isLegacyContract } from './contract-config.js';
