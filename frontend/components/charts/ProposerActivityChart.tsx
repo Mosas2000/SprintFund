fd73a106cb14df6bd56f2d4d489ff2836ce5d10b
0xMosas <tosinolosasa@gmail.com>
2026-04-30 09:14:03 +0100

    restore YAxis import in ProposerActivityChart

--- frontend/components/charts/ProposerActivityChart.tsx
+++ frontend/components/charts/ProposerActivityChart.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { BarChart, Bar, XAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 import { useGovernanceAnalytics } from '@/hooks/useGovernanceAnalytics';
 
 export default function ProposerActivityChart() {
